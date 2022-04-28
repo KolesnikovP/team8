4e2b41b27371307669d5b13dd1a10652f0d88f41
shah1dss <you@example.com>
2022-04-28 14:54:38 +0300

    release 2

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,13 +1,19 @@
 const userService = require('../service/user-service');
 
 class UserController {
-   async login(req, res, next) {
-     res.send('login')
+  //  async login(req, res, next) {
   //   try {
   //    res.send('login')
   //   } catch (e) {
   //     console.log(e)
   //   }
+  // }
+  async registration(req, res, next) {
+    try {
+     res.send('login')
+    } catch (e) {
+      console.log(e)
+    }
   }
 }
 
