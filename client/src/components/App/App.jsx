a2c90e4a4f6b9f348f8951f2ae57fbda3820a78c
Ilyak777 <ilya.kvasov.02@list.ru>
2022-04-29 13:58:33 +0300

    test Background resolve

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
 import { store } from '../../store';
 import Nav from '../Nav/Nav';
 import Login from '../Login/Login';
@@ -8,6 +9,23 @@ import FindForm from '../FindForm/FindForm';
 import style from './App.module.css';
 
 function App() {
+  // function changeBg() {
+  //   const pngs = [
+  //     'url("../../../public/img/dota1.png")',
+  //     'url("../../../public/img/dota1.png")',
+  //     'url("../../../public/img/dota1.png")',
+  //     'url("../../../public/img/dota1.png")',
+  //     'url(../../../public/img/terror.png)',
+  //     'url(../../../public/img/terror2.png)',
+  //   ];
+  //   const section = document.querySelector('*');
+  //   const bg = pngs[Math.floor(Math.random() * pngs.length)];
+
+  //   section.style.backgroundImage = bg;
+  // }
+  // useEffect(() => {
+  //   setInterval(changeBg, 1500);
+  // }, []);
   return (
     <div className={style.mainPage}>
       <BrowserRouter>
