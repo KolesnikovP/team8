6c491592149429d71b916bc84e6cac4c9387ee50
Alexey Zhirkov <avzhirkov@gmail.com>
2022-05-02 19:00:12 +0900

    fix adding Form

--- server/app.js
+++ server/app.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 const express = require('express');
-const sequelize = require('./db');
-const cookieSession = require("cookie-session");
+const cookieSession = require('cookie-session');
 const cors = require('cors');
-const passportSetup = require("./services/passport");
-const passport = require("passport");
+const passport = require('passport');
+const passportSetup = require('./services/passport');
+const sequelize = require('./db');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 const Router = require('./router/router');
-const authRoute = require("./router/auth");
+const authRoute = require('./router/auth');
 
 app.use(
-  cookieSession({ name: "session", keys: ["lama"], maxAge: 24 * 60 * 60 * 100 })
+  cookieSession({ name: 'session', keys: ['lama'], maxAge: 24 * 60 * 60 * 100 }),
 );
 
 app.use(passport.initialize());
@@ -21,13 +21,13 @@ app.use(passport.session());
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
-    methods: "GET,POST,PUT,DELETE",
+    methods: 'GET,POST,PUT,DELETE',
     credentials: true,
-  })
+  }),
 );
 
 app.use('/api', Router);
-app.use("/auth", authRoute);
+app.use('/auth', authRoute);
 
 const start = async () => {
   try {
