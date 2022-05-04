f4ae4b357ab193627cfef35e96c36b4e20b9ccc9
shah1dss <you@example.com>
2022-05-04 16:14:59 +0300

    rel0

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { Provider, useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useEffect } from 'react';
 import Nav from '../Nav/Nav';
@@ -10,7 +10,6 @@ import AccessForm from '../AccessForm/AccessForm';
 import { fetchUser } from '../../redux/thunk/user';
 import Profile from '../Profile/Profile';
 import PostList from '../PostList/PostList';
-import { store } from '../../store';
 
 function App() {
   const dispatch = useDispatch();
@@ -20,16 +19,16 @@ function App() {
   const { user } = useSelector((state) => state.userReducer);
   return (
     <BrowserRouter>
-        <Nav />
-        <div className={style.main}>
-          <Routes>
-            <Route path="/" element={<PostList />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/addPost" element={<FindForm />} />
-            <Route path="/accessForm" element={<AccessForm />} />
-            <Route path="/profile" element={<Profile user={user} />} />
-          </Routes>
-        </div>
+      <Nav />
+      <div className={style.main}>
+        <Routes>
+          <Route path="/" element={<PostList />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/addPost" element={<FindForm />} />
+          <Route path="/accessForm" element={<AccessForm />} />
+          <Route path="/profile" element={<Profile user={user} />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
