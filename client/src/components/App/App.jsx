984e5753e9d9aa740999653a1bf4c45b7e005101
shah1dss <you@example.com>
2022-05-04 23:15:03 +0300

    material ui1

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -2,6 +2,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useEffect } from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Nav1 from '../Nav/Nav1';
 import Login from '../Login/Login';
 import FindForm from '../FindForm/FindForm';
@@ -19,22 +20,30 @@ function App() {
     dispatch(fetchUser());
   }, [dispatch]);
   const { user } = useSelector((state) => state.userReducer);
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
   return (
     <BrowserRouter>
-      <Nav1 className={style.Nav} />
-      <body>
-        <div className={style.main}>
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/postList" element={user ? <PostList /> : <MainPage />} />
-            <Route path="/addPost" element={user ? <FindForm /> : <MainPage />} />
-            <Route path="/accessForm" element={user ? <AccessForm /> : <MainPage />} />
-            <Route path="/profile" element={user ? <Profile /> : <MainPage />} />
-          </Routes>
-        </div>
-      </body>
-      {/* <Footer className={style.Footer} /> */}
+      <ThemeProvider theme={darkTheme}>
+        <Nav1 className={style.Nav} />
+        <body>
+          <div className={style.main}>
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/postList" element={user ? <PostList /> : <MainPage />} />
+              <Route path="/addPost" element={user ? <FindForm /> : <MainPage />} />
+              <Route path="/accessForm" element={user ? <AccessForm /> : <MainPage />} />
+              <Route path="/profile" element={user ? <Profile /> : <MainPage />} />
+            </Routes>
+          </div>
+        </body>
+        {/* <Footer className={style.Footer} /> */}
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
--- client/src/components/Nav/Nav1.jsx
+++ client/src/components/Nav/Nav1.jsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import Avatar from '@mui/material/Avatar';
 import MenuItem from '@mui/material/MenuItem';
@@ -31,70 +29,68 @@ export default function MenuAppBar() {
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Box sx={{ mr: 2 }} onClick={() => navigate('/')}>
-            <img width="100px" className="logo" src="/img/logo.png" alt="" />
-          </Box>
-          {user.id ? (
-            <>
-              <Typography
-                variant="h6"
-                component="div"
-                sx={{ flexGrow: 0, marginRight: '1rem' }}
-                onClick={() => navigate('/addPost')}
-              >
-                создать заявку
-              </Typography>
-              <Typography
-                variant="h6"
-                component="div"
-                sx={{ flexGrow: 1 }}
-                onClick={() => navigate('/postList')}
-              >
-                посмотреть заявки
-              </Typography>
-            </>
-          ) : (
-            <Typography variant="h6" component="div" onClick={steam}>
-              Войти
-            </Typography>
-          )}
-          {user.id && (
-            <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <Avatar alt="Remy Sharp" src={user.steamAvatar} />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={() => navigate('/profile')}>Профиль</MenuItem>
-                <MenuItem onClick={logout}>Выйти</MenuItem>
-              </Menu>
-            </div>
-          )}
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="static" sx={{ backgroundColor: 'main' }}>
+      <Toolbar>
+        <MenuItem sx={{ mr: 2 }} onClick={() => navigate('/')}>
+          <img width="100px" className="logo" src="/img/logo.png" alt="" />
+        </MenuItem>
+        {user.id ? (
+          <>
+            <MenuItem
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 0, marginRight: '1rem' }}
+              onClick={() => navigate('/addPost')}
+            >
+              создать заявку
+            </MenuItem>
+            <MenuItem
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+              onClick={() => navigate('/postList')}
+            >
+              посмотреть заявки
+            </MenuItem>
+          </>
+        ) : (
+          <MenuItem variant="h6" component="div" onClick={steam}>
+            Войти
+          </MenuItem>
+        )}
+        {user.id && (
+          <div>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <Avatar alt="Remy Sharp" src={user.steamAvatar} />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={() => navigate('/profile')}>Профиль</MenuItem>
+              <MenuItem onClick={logout}>Выйти</MenuItem>
+            </Menu>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 }
