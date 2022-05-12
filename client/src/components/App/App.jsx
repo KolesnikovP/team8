54892c11548cf488e9496b0eb8dab26c53bd4fc9
shah1dss <you@example.com>
2022-05-12 13:41:29 +0300

    modal close on close

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -55,38 +55,14 @@ function App() {
   };
 
   return (
-    //     <BrowserRouter>
-    //       <ThemeProvider theme={darkTheme}>
-    //         {/* <Box sx={{ height: '100%', position: 'relative' }}> */}
-    //         <Navbar className={style.Nav} handelClickOpen={handelClickOpen} />
-    //         <Routes>
-    //           <Route path="/" element={user?.id ? <MainAuth /> : <MainPage />} />
-    //           {/* <Route path="/main" element={<MainAuth />} /> */}
-    //           <Route path="/login" element={<Login />} />
-    //           <Route path="/postList" element={user?.id ? <PostList /> : <Loader />} />
-    //           <Route path="/accessForm" element={user?.id ? <AccessForm /> : <Loader />} />
-    //           <Route exact path="/profile" element={user?.id ? <Profile /> : <Loader />} />
-    //           <Route exact path="/profile/:id" element={user?.id ? <LocalProfile /> : <Loader />} />
-    //           <Route exact path="/chat/:id" element={<WebSock user={user} />} />
-    //         </Routes>
-    //         <FindForm handleClose={handleClose} open={open} />
-    //         <Footer />
-    //         <DialogsButton handleClickOpenChat={handleClickOpenChat} />
-    //         <ChatFormModal handleCloseChat={handleCloseChat} openChat={openChat} user={user} />
-    //       </ThemeProvider>
-    //     </BrowserRouter>
-
     <Box sx={{ height: '100vh' }}>
       <BrowserRouter>
         <ThemeProvider theme={darkTheme}>
           <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-            {/* <Box sx={{ flex: '0 1 auto' }}> */}
             <Navbar handelClickOpen={handelClickOpen} />
             <Box
               sx={{
                 flex: '1 1 auto',
-                // background: '-webkit-linear-gradient(180deg, rgb(40, 44, 52) 28%, rgb(39, 39, 39))',
-                // background: '-moz-linear-gradient(180deg, rgb(40, 44, 52) 28%, rgb(39, 39, 39))',
                 background: 'linear-gradient(180deg, rgb(40, 44, 52) 28%, rgb(0, 0, 0))',
                 zIndex: '1',
               }}
@@ -109,7 +85,6 @@ function App() {
               <ChatFormModal handleCloseChat={handleCloseChat} openChat={openChat} user={user} />
             </Box>
             <Footer sx={{ flex: '0 0 auto' }} />
-            {/* </Box> */}
           </Box>
         </ThemeProvider>
       </BrowserRouter>
