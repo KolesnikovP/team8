6697379aefacb297ed568ffe7429f699a1acfae8
shah1dss <you@example.com>
2022-05-13 16:19:33 +0300

    deploy

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,15 +1,14 @@
-const router = require("express").Router();
-const passport = require("passport");
+const router = require('express').Router();
+const passport = require('passport');
 const UserController = require('../controllers/user-controller');
 
-router.get("/login/success", UserController.authSuccess);
-router.get("/login/failed", UserController.authFailed);
-router.get("/logout", UserController.authLogout);
-router.get("/steam", passport.authenticate("steam", { scope: ["profile"] }));
-router.get("/steam/callback",passport.authenticate("steam", {
-    successRedirect: 'http://localhost:3000/accessForm',
-    failureRedirect: "/login/failed",
-  })
-);
+router.get('/login/success', UserController.authSuccess);
+router.get('/login/failed', UserController.authFailed);
+router.get('/logout', UserController.authLogout);
+router.get('/steam', passport.authenticate('steam', { scope: ['profile'] }));
+router.get('/steam/callback', passport.authenticate('steam', {
+  successRedirect: `${process.env.CLIENT_URL}/accessForm`,
+  failureRedirect: '/login/failed',
+}));
 
-module.exports = router
+module.exports = router;
