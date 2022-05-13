177eb46703833c2a6733caaa74d21ea424f7d40d
Alexey Zhirkov <avzhirkov@gmail.com>
2022-05-13 11:33:28 +0300

    fix

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -109,16 +109,18 @@ function Profile() {
           {profGames.map((game, i) => (
             <Box sx={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', marginTop: '2rem' }} key={game.gameSteamId}>
               <Typography>{i + 1}</Typography>
-              <Typography variant="h4">{game.gameName}</Typography>
+              <Box sx={{ width: '300px', textAlign: 'center' }}>
+                <Typography variant="h4">{game.gameName}</Typography>
+              </Box>
               <img
                 src={`https://cdn.cloudflare.steamstatic.com/steam/apps/${game.gameSteamId}/header.jpg?t=1650992920`}
                 alt={`appIcon_${game.gameSteamId}`}
                 width="200px"
                 key={Date.now()}
-                />
-              <Box sx={{width: '300px', textAlign:'center'}}>
-              <Typography>Количество часов: {game.userGameHours}</Typography>
-                </Box>
+              />
+              <Box sx={{ width: '300px', textAlign: 'center' }}>
+                <Typography>Количество часов: {game.userGameHours}</Typography>
+              </Box>
             </Box>
           ))}
           <Box>
