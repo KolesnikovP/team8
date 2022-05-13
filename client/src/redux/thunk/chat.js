778784d2a390b0b995b50377f3f260e5eaee9a1c
Alexey Zhirkov <avzhirkov@gmail.com>
2022-05-13 11:15:33 +0300

    some fix

--- client/src/redux/thunk/chat.js
+++ client/src/redux/thunk/chat.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* eslint-disable no-restricted-syntax */
 function filterByProp(arr1, prop) {
   const seen = {};
@@ -11,19 +10,6 @@ function filterByProp(arr1, prop) {
   });
   return result;
 }
-=======
-// function unique(arr) {
-//   let result = [];
-
-//   for (let str of arr) {
-//     if (!result.includes(str)) {
-//       result.push(str);
-//     }
-//   }
-
-//   return result;
-// }
->>>>>>> c121feacc63de88387567b272b80f16f5c69abaf
 
 export const getNews = (id, func) => {
   fetch(`http://localhost:4000/api/getHistory`, {
@@ -35,12 +21,6 @@ export const getNews = (id, func) => {
   })
     .then((response) => response.json())
     .then((data) => {
-<<<<<<< HEAD
       func(filterByProp(data, 'idSms'));
-=======
-      // console.log(data);
-      // unique(data);
-      func(data);
->>>>>>> c121feacc63de88387567b272b80f16f5c69abaf
     });
 };
